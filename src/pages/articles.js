cf21635043ced9d6a2e4f177554f67d1d7dcdce5
Marta <marta.nunes.abreu@gmail.com>
2020-10-31 18:08:11 +0000

    added dummy articles page

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,16 +1,110 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const Subheader = styled.div`
+  margin: auto;
+  max-width: 500px;
+
+`
+
+const YearHeading = styled.div`
+  margin-top: 30px;
+`
+
+const ArticleHeading = styled.div`
+  border: 1px solid gray;
+  border-radius: 5px;
+  margin: 10px 0;
+  padding: 10px 5px;
+  padding-left: 10px;
+  display: flex;
+  align-items: center;
+
+  .article-title{
+    font-size: 22px;
+    text-align: left;
+    flex: 1;
+  }
+  .tag-javascript {
+    font-size: 14px;
+    padding: 2px 5px;
+    background-color: yellow;
+    border-radius: 5px;
+    margin: 0 1px;
+  }
+  .tag-python {
+    font-size: 14px;
+    padding: 2px 5px;
+    background-color: green;
+    border-radius: 5px;
+    margin: 0 1px;
+  }
+  .tag-project {
+    font-size: 14px;
+    margin: 0 1px;
+    padding: 2px 5px;
+    background-color: orange;
+    border-radius: 5px;
+  }
+  .tag-c {
+    font-size: 14px;
+    margin: 0 1px;
+    padding: 2px 5px;
+    background-color: blue;
+    border-radius: 5px;
+  }
+`
+
 
 const Articles = () => (
   <Layout>
     <SEO title="Articles"/>
       <h1>Articles</h1>
-      <p>A compilation of single-topic posts, tutorials, complete project write-ups and everything in between.</p>
-      <p>Search keywords or phrases to find relevant posts.</p>
-      <Link to="/">Go back to the homepage</Link>
+      <Subheader>
+        <p>A compilation of single-topic posts, tutorials, complete project write-ups and everything in between.</p>
+        <p>Search keywords or phrases to find relevant posts.</p>
+      </Subheader>
+
+      <YearHeading></YearHeading>
+      <ArticleHeading>
+        <p class="article-title">Learning Javascript as a C Developer</p>
+        <p class="tag-javascript">Javascript</p>
+      </ArticleHeading>
+      <ArticleHeading>
+        <p class="article-title">Categorising images with openCV</p>
+        <p class="tag-python">Python</p>
+      </ArticleHeading>
+      <ArticleHeading>
+        <p class="article-title">Understanding speech with NLP</p>
+        <p class="tag-python">Python</p>
+      </ArticleHeading>
+      <ArticleHeading>
+        <p class="article-title">Raspberry Pi as a smart home monitor</p>
+        <p class="tag-python">Python</p>
+        <p class="tag-project">Project</p>
+      </ArticleHeading>
+      <ArticleHeading>
+        <p class="article-title">Authenticating with Flask</p>
+        <p class="tag-python">Python</p>
+      </ArticleHeading>
+      <ArticleHeading>
+        <p class="article-title">Integrating Flask and SQLit</p>
+        <p class="tag-python">Python</p>
+      </ArticleHeading>
+      <ArticleHeading>
+        <p class="article-title">Voice assistant with an ESP32 microchip</p>
+        <p class="tag-c">C</p>
+        <p class="tag-project">Project</p>
+      </ArticleHeading>
+      <ArticleHeading>
+        <p class="article-title">What to know about wireless comms</p>
+        <p class="tag-c">C</p>
+        <p class="tag-project">IoT</p>
+      </ArticleHeading>
+
   </Layout>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,6 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import styled from "styled-components"
 
-
-const CustomMain = styled.main`
-
-`
 const CustomHeader = styled.div`
   padding: 5px 5px;
   margin: 5px 0;
@@ -33,27 +29,25 @@ const CustomBody = styled.div`
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <CustomMain>
-      <CustomHeader>
+    <CustomHeader>
         <Title>Hi there, I'm Marta</Title>
         <Subtitle>
           <p>A software engineer with an enthusiasm for IoT and data security.</p>
           <p>This space is intended to store my ideas and ongoing learning progress, as well to distribute any helpful advice and past experience.</p>
         </Subtitle>
-      </CustomHeader>
-      <CustomBody>
-        <h3>Latest articles</h3>
-        <p>Insert content here </p>
+    </CustomHeader>
+    <CustomBody>
+      <h3>Latest articles</h3>
+      <p>Insert content here </p>
 
-        <h3>Projects</h3>
-        <p>Insert content here </p>
+      <h3>Projects</h3>
+      <p>Insert content here </p>
 
-        <h3>Personal posts</h3>
-        <p>Things I'm learning at the moment</p>
-        <p>Things I've learned this month</p>
-        <p>Books I recommend</p>
-      </CustomBody>
-      </CustomMain>
+      <h3>Personal posts</h3>
+      <p>Things I'm learning at the moment</p>
+      <p>Things I've learned this month</p>
+      <p>Books I recommend</p>
+    </CustomBody>
   </Layout>
 )
 
